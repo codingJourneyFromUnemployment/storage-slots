76497cc5cf6d54be77b62ec0b9128afccfc9f2d8
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2023-07-22 21:00:45 +0800

    32:18

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -2,19 +2,23 @@
 pragma solidity ^0.8.4;
 
 // Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
+import "./StorageSlot.sol";
 
 contract Storage {
-    uint256 x = 97; //0x0
-    uint256 y = 56; //0x1
+    // uint256 x = 97; //0x0
+    // uint256 y = 56; //0x1
 
-    mapping (uint => uint) test; //0x2 
+    // mapping (uint => uint) test; //0x2 
+    // constructor() {
+    //     // keccak256(11 + 0x2)
+    //     test[11] = 1;
+    //     // keccak256(22 + 0x2)
+    //     test[22] = 10;
+    //     // keccak256(33 + 0x2)
+    //     test[33] = 20;
+    // }
     constructor() {
-        // keccak256(11 + 0x2)
-        test[11] = 1;
-        // keccak256(22 + 0x2)
-        test[22] = 10;
-        // keccak256(33 + 0x2)
-        test[33] = 20;
+        StorageSlot.getUint256Slot(keccak256("beidi")).value = 17;
     }
 }
--- scripts/lookup.js
+++ scripts/lookup.js
@@ -1,18 +1,24 @@
 const hre = require("hardhat");
 
-const addr = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9"
+const addr = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 async function lookup() {
   // eth_getStorageAt
-  const key = 21;
-  const baseSlot = 0x2;
-  const slot = keccak256(key + baseSlot)
-  const value0 = await hre.ethers.provider.getStorage(addr, 0)
-  console.log(`Storage at ${addr} is ${value0}`);
-  const value1 = await hre.ethers.provider.getStorage(addr, 1)
-  console.log(`Storage at ${addr} is ${value1}`);
-  const value2 = await hre.ethers.provider.getStorage(addr, 2)
-  console.log(`Storage at ${addr} is ${value2}`);
+  // const key = hre.ethers.zeroPadValue(hre.ethers.toBeArray(33), 32)
+  // const baseSlot = hre.ethers.zeroPadValue(hre.ethers.toBeArray(0x2), 32)
+  // console.log({key, baseSlot})
+  // const slot = hre.ethers.keccak256(key + baseSlot.substring(2))
+  // const value0 = await hre.ethers.provider.getStorage(addr, 0)
+  // console.log(`Storage at ${addr} is ${value0}`);
+  // const value1 = await hre.ethers.provider.getStorage(addr, 1)
+  // console.log(`Storage at ${addr} is ${value1}`);
+  // const value2 = await hre.ethers.provider.getStorage(addr, slot)
+  // console.log(`Storage at ${addr} is ${value2}`);
+
+  const key = hre.ethers.toUtf8Bytes("beidi");
+  const slot = hre.ethers.keccak256(key)
+  const value = await hre.ethers.provider.getStorage(addr, slot)
+  console.log(`Storage at ${addr} is ${value}`);
 }
 
 lookup();
